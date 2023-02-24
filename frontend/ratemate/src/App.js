08257442468a891ee46224bf89ce317110378bc7
Jason Le <jasonctle@gmail.com>
2023-02-23 16:32:36 -0800

    Added base code for frontend

--- frontend/ratemate/src/App.js
+++ frontend/ratemate/src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import HelloWorld from "./components/HelloWorld";
+import ReviewForm from "./components/ReviewForm";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HelloWorld />,
+    children: [],
+  },
+  {
+    path: "/leavereview",
+    element: <ReviewForm />,
+    children: [],
+  },
+]);
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ frontend/ratemate/src/components/ReviewForm.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ReviewForm = () => {
+  return (
+    <div>
+      <h1>Hello World</h1>
+    </div>
+  );
+};
+
+export default ReviewForm;
